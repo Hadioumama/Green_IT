019da0402b2864fb184aac932de4be78443f9f26
Hadi oumama <hadi.oumama@gmail.com>
2026-05-10 19:29:51 +0100

    partie inscription utilisateur

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function showRegister()
+    {
+        return view('register');
+    }
+
+    public function register(Request $request)
+    {
+        // 1. validation
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+        ]);
+
+        // 2. création utilisateur
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'user'
+        ]);
+
+        // 3. redirection
+        return redirect('/login')->with('success', 'Compte créé avec succès');
+    }
+}
